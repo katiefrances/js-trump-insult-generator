e042cfdbec3ad2178b31117afe9834b9033740a9
Katie Frances <katiebeeisdead@gmail.com>
2018-06-23 17:42:00 +1000

    Add even more insults

--- index.js
+++ index.js
@@ -3,11 +3,10 @@
 
 const wordList = [
     "cheeto faced monster", "unbelievably incompetent bufoon", 
-    "the smartest guy in the room everybody says so", "about to be impeached idiot",
+    "the smartest guy in the room; everybody says so", "about to be impeached idiot",
     "racist left over chicken tikka", "I don't care about refugees, do u?",
-    
+    "extremely talented accordion player", "winner of bankruptcy contests"
 ]
 
-let result = wordList[Math.floor(Math.random()*wordList.length)];
-
+let result = wordList[Math.floor(Math.random()*wordList.length)]
 console.log(result)
