f29104d03d5d436e7ffba95d2559c09e82752fa5
Katie Frances <katiebeeisdead@gmail.com>
2018-06-23 17:17:45 +1000

    Add more insults

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // app that changes a phrase to a random element in an array eg math.random
-// let's try to copy Detrumpify 
+// let's try to copy Detrumpify!
 
 const wordList = [
-    "cheeto faced monster", "unbelievably incompetent bufoon", "the smartest guy in the room everybody says so", ""
+    "cheeto faced monster", "unbelievably incompetent bufoon", 
+    "the smartest guy in the room everybody says so", "about to be impeached idiot",
+    "racist left over chicken tikka", "I don't care about refugees, do u?",
+    
 ]
-
-
 
 let result = wordList[Math.floor(Math.random()*wordList.length)];
 
