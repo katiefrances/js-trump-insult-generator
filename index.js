c0ef0dbdf4db2f214e151b2196028c641445021a
Katie Frances <katiebeeisdead@gmail.com>
2019-06-02 17:31:28 +1000

    Add Bootstrap

--- index.js
+++ index.js
@@ -2,12 +2,24 @@
 // let's try to replicate Detrumpify!
 
 const wordList = [
-    "cheeto faced monster", "unbelievably incompetent bufoon", 
-    "the smartest guy in the room; everybody says so", "about to be impeached idiot",
-    "racist left over chicken tikka", "I don't care about refugees, do u?",
-    "extremely talented accordion player", "winner of bankruptcy contests",
-    "flamboyant dumpster makeover", "the dolt in cheif"
-]
+  "cheeto faced monster",
+  "unbelievably incompetent bufoon",
+  "the smartest guy in the room; everybody says so",
+  "about to be impeached idiot",
+  "racist left over chicken tikka",
+  "I don't care about refugees, do u?",
+  "extremely talented accordion player",
+  "winner of bankruptcy contests",
+  "flamboyant dumpster makeover",
+  "the dolt in cheif"
+];
 
-let result = wordList[Math.floor(Math.random()*wordList.length)]
-console.log(result)
+let result = wordList[Math.floor(Math.random() * wordList.length)];
+console.log(result);
+
+// let element = document.getElementById(display);
+
+function changeColor(newColor) {
+  var elem = document.getElementById("para");
+  elem.style.color = newColor;
+}
